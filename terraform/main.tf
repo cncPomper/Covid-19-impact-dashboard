bd17c0ef7baba05135ded45df9ee5b700c6a7065
Piotr Kitłowski <piotr.kitlowski@gmail.com>
2024-03-25 18:34:11 +0000

    Configured main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,44 @@
 terraform {
-    required_version = ">= 1.0"
-    backend "local" {}  
-    required_providers {
-        google = {
-            source  = "hashicorp/google"
-        }
+  required_version = ">= 1.0"
+  backend "local" {}
+  required_providers {
+    google = {
+      source = "hashicorp/google"
     }
+  }
 }
 
 provider "google" {
-    project = var.project
-    region = var.region
+  project     = var.project
+  region      = var.region
+  credentials = file(var.credentials)
 }
 
-resource "google
+resource "google_bigquery_dataset" "covid_project_dataset" {
+  dataset_id = var.BQ_dataset_name
+  location   = var.location
+}
+
+resource "google_storage_bucket" "covid_data_lake_bucket" {
+  name     = "${var.project}_235_project" # Concatenating DL bucket & Project name for unique naming
+  location = var.region
+
+  # Optional, but recommended settings:
+  storage_class               = var.GCS_storage_class
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30 // days
+    }
+  }
+
+  force_destroy = true
+}
